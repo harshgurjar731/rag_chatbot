3bb4bce6d6af802e9d12ede82191d35c7a4693a5
Yash Gupta <77715788+YASHGUPTA9063@users.noreply.github.com>
2026-02-06 13:23:41 +0000

    small changes

--- Backend/main.py
+++ Backend/main.py
@@ -73,11 +73,6 @@ configure_opentelemetry_for_phoenix()
 from database import init_db, list_tables, list_tables_content, list_file_content
 # from routes import upload, preview, chunking, embedding, retriever, delete, url_scraper, translate, frontend_config
 import requests
-from ingestion_pipleline.ingestion_datastore_router import router as ingestion_datastore_router
-from ingestion_pipleline.ingestion_document_loader_router import router as ingestion_document_router
-from ingestion_pipleline.ingestion_chunks_router import router as ingestion_chunking_router
-from ingestion_pipeline.secondary_source_router import router as secondary_source_router # Added
-from rag_pipeline.rag_router import router as rag_knowledge_asst_router
 from fastapi.responses import Response
 
 
@@ -101,6 +96,11 @@ async def preflight_handler(full_path: str):
     """Handle preflight requests for all routes"""
     return Response(status_code=200)
 
+from ingestion_pipleline.ingestion_datastore_router import router as ingestion_datastore_router
+from ingestion_pipleline.ingestion_document_loader_router import router as ingestion_document_router
+from ingestion_pipleline.ingestion_chunks_router import router as ingestion_chunking_router
+from ingestion_pipeline.secondary_source_router import router as secondary_source_router # Added
+from rag_pipeline.rag_router import router as rag_knowledge_asst_router
 from routes import translate, frontend_config, embedding
 # # Include all the different API routers
 # app.include_router(datastore.router, prefix="/datastore")
